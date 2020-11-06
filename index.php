2ced75f02da485d80f1b3f6affa73d4d025ff8cd
Sarawutgame <73683067+Sarawutgame@users.noreply.github.com>
2020-11-06 19:29:56 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>About Sea</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+<body style="background-color:rgba(226, 244, 245, 0.89);">
+<div class="container">
+    <div class="row">
+        <div class="col-sm-4">
+            <div class="card">
+                <img class="card-img-top" src="img/life/life2.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <div class="collapse-content">
+                    <h4 class="card-title">Yello Van On </h4>
+                    <p class="card-text"><small class="text-muted">My soul is full of longingfor the secret of the sea, and the heart of the great oceansends a thrilling pulse through me.</small></p>
+                    <i class="fa fa-heart-o text-muted float-right p-1 my-1 mr-3" data-toggle="tooltip" data-placement="top" title="STW"></i>
+                    <img src="img/profile.png" class="card-img-bottom" style="height: 40px; width: 40px;" />
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4">
+            <div class="card">
+                <img class="card-img-top" src="img/life/life3.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <div class="collapse-content">
+                    <h4 class="card-title">To Sea!!!</h4>
+                    <p class="card-text"><small class="text-muted">She loves the serene brutality of the ocean, loves the electric power she felt with each breath of wet, briny air the deep.</small></p>
+                    <i class="fa fa-heart-o text-muted float-right p-1 my-1 mr-3" data-toggle="tooltip" data-placement="top" title="STW"></i>
+                    <img src="img/profile.png" class="card-img-bottom" style="height: 40px; width: 40px;" />
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4">
+            <div class="card">
+                <img class="card-img-top" src="img/life/life4.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <div class="collapse-content">
+                    <h4 class="card-title">Cross Road</h4>
+                    <p class="card-text"><small class="text-muted">My city isn’t a bad city however it’s nothing to get too excited about either. Because it’s a quiet city lots of people move here to start</small></p>
+                    <i class="fa fa-heart-o text-muted float-right p-1 my-1 mr-3" data-toggle="tooltip" data-placement="top" title="STW"></i>
+                    <img src="img/profile.png" class="card-img-bottom" style="height: 40px; width: 40px;" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
